f75ae731ac4b06610ecae5ce3380c155543cc4bc
Ladislav Imrišek <ladislav.i@azet.sk>
2021-05-07 15:25:47 +0200

    Verzia 1.1 všetky metódy spracované aj neautorizované aj autorizované. môže sa zmerdžovať s main branch

--- src/main/java/sk/fri/uniza/App.java
+++ src/main/java/sk/fri/uniza/App.java
@@ -43,58 +43,15 @@ public class App {
             e.printStackTrace();
         }
 
-
-        // Vytvorenie požiadavky na získanie údajov o všetkých meteo staniciach
-        Call<List<Location>> stationLocations =
-                iotNode.getWeatherStationService().getStationLocations();
-
-
-        try {
-            Response<List<Location>> response = stationLocations.execute();
-
-            if (response.isSuccessful()) { // Dotaz na server bol neúspešný
-                //Získanie údajov vo forme Zoznam lokacií
-                List<Location> body = response.body();
-
-                System.out.println(body);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        final Call<WeatherData> weather_asClass = iotNode.getWeatherStationService().getCurrentWeather("station_1");
-
-
-        try {
-            Response<WeatherData> responseAsClass = weather_asClass.execute();
-            if(responseAsClass.isSuccessful()){
-                final WeatherData body = responseAsClass.body();
-                System.out.println(body);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        final Call<List<WeatherData>> hist_1 = iotNode.getWeatherStationService().getHistoryWeather("station_1","3/05/2021 15:00", "6/05/2021 15:00");
-
-        try {
-            final Response<List<WeatherData>> response_hist = hist_1.execute();
-            if(response_hist.isSuccessful()){
-                final List<WeatherData> body = response_hist.body();
-                System.out.println(body);
-            }else{
-                System.out.println("Chyba pri načítanie historických dát:"+response_hist.errorBody().string());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        iotNode.meteoData_of_stations_noAuth();
+        iotNode.curWeather_noAuth("station_3");
+        iotNode.historyNoAuth("station_1","6/05/2021 14:00", "6/05/2021 16:00");
         iotNode.getAverageTemperature("station_1","6/05/2021 14:00", "6/05/2021 16:00");
-
-        System.out.println("\ntoken je:"+iotNode.getToken().getToken());
-
+        iotNode.getToken(); //  získa sa zároveň nastaví.
         iotNode.getLocationsOfStation();
+        iotNode.getAuthWeatherAtStation("station_2");
+        iotNode.getAuthWeatherAtStation("station_2",List.of("time","date"));
+        iotNode.getAuthHistory_Weather("station_2","3/05/2021 15:00", "6/05/2021 15:00");
+        iotNode.getAuthHistory_Weather("station_2","3/05/2021 15:00", "6/05/2021 15:00",List.of("time", "date"));
     }
 }
--- src/main/java/sk/fri/uniza/IotNode.java
+++ src/main/java/sk/fri/uniza/IotNode.java
@@ -98,7 +98,7 @@ public class IotNode {
 
              if(responseLoc.isSuccessful()){
                 body = responseLoc.body();
-                 System.out.println("\n Autorizovaný prístup:\n");
+                 System.out.println("\n Miesta staníc ->Autorizovaný prístup:\n");
                  System.out.println(body.toString());
              }else{
                  System.out.println(responseLoc.errorBody().string());
@@ -109,4 +109,189 @@ public class IotNode {
          }
          return body;
      }
+
+     public WeatherData getAuthWeatherAtStation(String station){
+         WeatherData body = null;
+
+         final Call<WeatherData> currentWeatherAuth = getWeatherStationService().getCurrentWeatherAuth(aToken.getToken(), station);
+         try {
+             final Response<WeatherData> responseWeath= currentWeatherAuth.execute();
+             if(responseWeath.isSuccessful()){
+                 body = responseWeath.body();
+                 System.out.println("\n Aktuálne počasie na stanici-> Autorizovaný prístup:\n");
+                 System.out.println(body.toString());
+             }else{
+                 System.out.println(responseWeath.errorBody().string());
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         return body;
+     }
+
+     public WeatherData getAuthWeatherAtStation(String station, List<String> fields){
+         WeatherData body = null;
+
+         final Call<WeatherData> currentWeatherAuth = getWeatherStationService().getCurrentWeatherAuth(aToken.getToken(), station,fields);
+         try {
+             final Response<WeatherData> responseWeath= currentWeatherAuth.execute();
+             if(responseWeath.isSuccessful()){
+                 body = responseWeath.body();
+                 System.out.println("\n Aktuálne počasie na stanici, vybrane udaje-> Autorizovaný prístup:\n");
+                 System.out.println(body.toString());
+             }else{
+                 System.out.println(responseWeath.errorBody().string());
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         return body;
+     }
+
+
+    public List<WeatherData> getAuthHistory_Weather(String station, String from, String to){
+        List<WeatherData> body = null;
+
+        final Call<List<WeatherData>> historyWeatherAuth = getWeatherStationService().getHistoryWeatherAuth(aToken.getToken(), station, from, to);
+        try {
+            final Response<List<WeatherData>> responseHist = historyWeatherAuth.execute();
+            if(responseHist.isSuccessful()){
+                body = responseHist.body();
+                System.out.println("\n História počasia na stanici od do -> Autorizovaný prístup:\n");
+                System.out.println(body.toString());
+            }else{
+                System.out.println(responseHist.errorBody().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+
+    public List<WeatherData> getAuthHistory_Weather(String station, String from, String to, List<String> fields){
+        List<WeatherData> body = null;
+
+        final Call<List<WeatherData>> historyWeatherAuth = getWeatherStationService().getHistoryWeatherAuth(aToken.getToken(), station, from, to,fields);
+        try {
+            final Response<List<WeatherData>> responseHist = historyWeatherAuth.execute();
+            if(responseHist.isSuccessful()){
+                body = responseHist.body();
+                System.out.println("\n História počasia od do, vybrané údaje -> Autorizovaný prístup:\n");
+                System.out.println(body.toString());
+            }else{
+                System.out.println(responseHist.errorBody().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+
+
+    public List<WeatherData> historyNoAuth(String station, String from, String to){
+
+        List<WeatherData> body = null;
+
+        final Call<List<WeatherData>> hist_1 = getWeatherStationService().getHistoryWeather(station,from,to);
+
+        try {
+            final Response<List<WeatherData>> response_hist = hist_1.execute();
+            if(response_hist.isSuccessful()){
+                body = response_hist.body();
+                System.out.println(body);
+            }else{
+                System.out.println("Chyba pri načítanie historických dát:"+response_hist.errorBody().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return body;
+
+    }
+
+    public List<WeatherData> historyNoAuth(String station, String from, String to, List<String> fields){
+
+        List<WeatherData> body = null;
+
+        final Call<List<WeatherData>> hist_1 = getWeatherStationService().getHistoryWeather(station,from,to,fields);
+
+        try {
+            final Response<List<WeatherData>> response_hist = hist_1.execute();
+            if(response_hist.isSuccessful()){
+                body = response_hist.body();
+                System.out.println(body);
+            }else{
+                System.out.println("Chyba pri načítanie historických dát:"+response_hist.errorBody().string());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return body;
+
+    }
+
+    public WeatherData curWeather_noAuth(String station){
+
+        WeatherData body = null;
+        final Call<WeatherData> weather_asClass =  getWeatherStationService().getCurrentWeather( station);
+
+
+        try {
+            Response<WeatherData> responseAsClass = weather_asClass.execute();
+            if(responseAsClass.isSuccessful()){
+                body = responseAsClass.body();
+                System.out.println(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+
+    public List<Location> meteoData_of_stations_noAuth(){
+
+        List<Location> body = null;
+        Call<List<Location>> stationLocations = getWeatherStationService().getStationLocations();
+
+        try {
+            Response<List<Location>> response = stationLocations.execute();
+
+            if (response.isSuccessful()) { // Dotaz na server bol neúspešný
+                //Získanie údajov vo forme Zoznam lokacií
+                body = response.body();
+                System.out.println(body);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return body;
+    }
+
+//    public Object universal(Object o1){
+//
+//        try {
+//            final Response<WeatherData> responseWeath= o1.execute();
+//            if(responseWeath.isSuccessful()){
+//                body = responseWeath.body();
+//                System.out.println("\n Autorizovaný prístup:\n");
+//                System.out.println(body.toString());
+//            }else{
+//                System.out.println(responseWeath.errorBody().string());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
 }
+
+
+
